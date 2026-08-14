f4122aeb5cbae7316620bae9041070ab213f466a
GUARAFLOW <jhonatang.morenop@gmail.com>
2026-08-14 18:28:02 -0500

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ conn.commit()
 
 # --- TÍTULO PRINCIPAL ---
 st.title("💳 CREDITOS ROTATIVOS BANKCALI")
-st.image("LOGOBANKCALI", use_column_width=True)
+st.image("LOGOBANKCALI.jpeg", use_column_width=True)
 
 # --- CONTROL DE PERMISOS / ROLES EN BARRA LATERAL ---
 st.sidebar.title("🔐 Control de Acceso")
