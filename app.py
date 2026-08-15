2190de116b5f8a783b1f7024c5913190ee10a9b5
GUARAFLOW <jhonatang.morenop@gmail.com>
2026-08-15 11:36:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -816,16 +816,16 @@ elif opcion == "3. Registrar Pagos / Abonar Cuotas" and es_admin:
       col_p2.info(f"📌 **Valor Cuota Sugerido:** ${vlr_cuota:,.0f} COP")
 
       # Ajustamos los valores para que nunca den error cuando el saldo sea 0
-min_permitido = 0.0 if saldo_act <= 0 else 1.0
-val_sugerido = max(min_permitido, float(min(vlr_cuota, saldo_act)))
+      min_permitido = 0.0 if saldo_act <= 0 else 1.0
+      val_sugerido = max(min_permitido, float(min(vlr_cuota, saldo_act)))
 
-monto_abono = st.number_input(
-    "Monto del Abono ($ COP)",
-    min_value=min_permitido,
-    max_value=max(min_permitido, float(saldo_act)),
-    value=val_sugerido,
-    step=1000.0
-)
+      monto_abono = st.number_input(
+      "Monto del Abono ($ COP)",
+      min_value=min_permitido,
+      max_value=max(min_permitido, float(saldo_act)),
+      value=val_sugerido,
+      step=1000.0
+       )
 
       if st.button("💾 Registrar Pago Oficial", use_container_width=True):
         fecha_pago = datetime.now().strftime("%Y-%m-%d %H:%M")
